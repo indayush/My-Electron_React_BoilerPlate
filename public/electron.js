a1bdfbf1d4f30e6e9046ea772a2987740a7d0feb
Ayush <ayushadarsh03@gmail.com>
2021-01-29 18:17:56 +0530

    Launched React inside Electron

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron')
+const path = require("path")
+const isDev = require("electron-is-dev")
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -10,7 +12,10 @@ function createWindow () {
   })
 
   win.loadURL(
-      'index.html'
+      isDev ? 
+      "http://localhost:3000" 
+      :
+      `file://${path.join(__dirname, "../build/index.html")}`
     )
 }
 
